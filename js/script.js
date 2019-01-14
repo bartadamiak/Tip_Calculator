a5e5ba926972086cfd3fbdbe7a4d1cc8866a5324
bartadam <bart.adamiak@gmail.com>
2019-01-14 17:28:00 +0100

    Different kind of tips

--- js/script.js
+++ js/script.js
@@ -8,26 +8,36 @@ function CalculateTip() {
     let billVal = parseFloat(document.querySelector('#bill').value);
     let peopleVal = parseFloat(document.querySelector('#people').value);
     let qualityVal = document.querySelector('#quality').value;
-    let qualitySum = document.querySelector('.qualitySummary p');
+    let qualitySum = document.querySelector('.qualitySummary div');
+
+    let tip = 1;
 
 
     if (qualityVal === "excellent") {
-        qualitySum.innerText = "25%"
+        qualitySum.innerText = "25%";
+        tip = (billVal*25)/100
     };
     if (qualityVal === "ok") {
-        qualitySum.innerText = "15%"
+        qualitySum.innerText = "15%";
+        tip = (billVal*15)/100
     };
     if (qualityVal === "extremelyBad") {
-        qualitySum.innerText = "0%"
+        qualitySum.innerText = "0%";
+        tip = 0
     }
 
     
-    let bill = document.querySelector('.billSummary p').innerText = billVal;
-    let tip = (billVal*15)/100;
-    let tipSum = document.querySelector('.tip p').innerText = tip;
-    let tipPerPerson = document.querySelector('.tipPerPerson p').innerText = tip/peopleVal;
+    let bill = document.querySelector('.billSummary div').innerText = billVal;
+    
+
+    
+
+
+
+    let tipSum = document.querySelector('.tip div').innerText = tip;
+    let tipPerPerson = document.querySelector('.tipPerPerson div').innerText = tip/peopleVal;
     let billPerPerson = billVal/peopleVal;
-    let billPerPersonSum = document.querySelector('.billPerPerson p').innerText = billPerPerson;
+    let billPerPersonSum = document.querySelector('.billPerPerson div').innerText = billPerPerson;
     
 
 
